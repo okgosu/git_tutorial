bfcd24ea3ee41806d13422b046f054b66604c963
okgosu <okgosu@gmail.com>
2019-05-12 16:40:09 +0900

    4th mod

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 // For command line git tutorial
 // First: Local repository modification
 // Second: Github modification 
+// Forth : test mod 
 print("hello world")
 print("Tell me about your world")
 print("Tell his world")
